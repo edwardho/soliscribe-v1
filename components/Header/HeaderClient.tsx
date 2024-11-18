20d0bec53d683a92ddfffab444a2047e2ff007af
Edward Ho <ekinbenho@gmail.com>
2024-11-17 22:23:59 -0500

    auth callback updates

--- components/Header/HeaderClient.tsx
+++ components/Header/HeaderClient.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import ThemeToggler from "./ThemeToggler";
 import Image from "next/image";
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import MobileNav from "./MobileNav";
 
 export default function HeaderClient({ 
   children 
@@ -63,45 +64,6 @@ export default function HeaderClient({
                 />
                 <span className="text-lg font-semibold text-foreground">soliscribe</span>
               </a>
-
-              {/* Hamburger Button */}
-              <button
-                aria-label="hamburger Toggler"
-                className="ml-4 block xl:hidden"
-                onClick={() => setNavigationOpen(!navigationOpen)}
-              >
-                <span className="relative block h-5.5 w-5.5 cursor-pointer">
-                  <span className="absolute right-0 block h-full w-full">
-                    <span
-                      className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-foreground delay-[0] duration-200 ease-in-out ${
-                        !navigationOpen ? "!w-full delay-300" : "w-0"
-                      }`}
-                    ></span>
-                    <span
-                      className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-foreground delay-150 duration-200 ease-in-out ${
-                        !navigationOpen ? "delay-400 !w-full" : "w-0"
-                      }`}
-                    ></span>
-                    <span
-                      className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-foreground delay-200 duration-200 ease-in-out ${
-                        !navigationOpen ? "!w-full delay-500" : "w-0"
-                      }`}
-                    ></span>
-                  </span>
-                  <span className="du-block absolute right-0 h-full w-full rotate-45">
-                    <span
-                      className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-foreground delay-300 duration-200 ease-in-out ${
-                        !navigationOpen ? "!h-0 delay-[0]" : "h-full"
-                      }`}
-                    ></span>
-                    <span
-                      className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-foreground duration-200 ease-in-out ${
-                        !navigationOpen ? "!h-0 delay-200" : "h-0.5"
-                      }`}
-                    ></span>
-                  </span>
-                </span>
-              </button>
             </div>
 
             <div
@@ -163,6 +125,7 @@ export default function HeaderClient({
             <div className="flex items-center gap-5">
               <ThemeToggler />
               {children}
+              <MobileNav isAuth={!!user} />
             </div>
           </div>
         </div>
